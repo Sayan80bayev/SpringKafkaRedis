0c2862b212817a2e1cf7faadef4eac717fa45a0f
sayan08bayev <sayan123serv@gmail.com>
2024-08-11 21:23:24 +0600

    duration example

--- src/main/java/com/example/demo/config/CacheConfig.java
+++ src/main/java/com/example/demo/config/CacheConfig.java
@@ -1,8 +1,11 @@
 package com.example.demo.config;
 
+import java.time.Duration;
+
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
@@ -11,6 +14,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 public class CacheConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        return RedisCacheManager.create(connectionFactory);
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(3))
+                .disableCachingNullValues();
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(cacheConfiguration).build();
     }
 }
